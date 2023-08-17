eb23d3193f1ad2dad6294f1b62e3f9c680c82067
GuilhermeTSouza <gts12ou1@gmail.com>
2023-08-17 17:17:16 -0300

    Tarefa Api criar e deletar tarefas

---
+++ todolist/src/main/java/br/com/todolist/TodoController.java
@@ -0,0 +1,41 @@
+package br.com.todolist;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+    private final TodoRepository todoRepo;
+
+    public TodoController(TodoRepository todoRepo) {
+        this.todoRepo = todoRepo;
+    }
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    public List <Todo> getAll() {
+        return this.todoRepo.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Todo crete(@RequestBody Todo tarefa){
+        return this.todoRepo.save(tarefa);
+    }
+
+    @DeleteMapping("/{tarefaId}")
+    public ResponseEntity<Void> delete(@PathVariable Integer tarefaId){
+        Optional<Todo> todo = this.todoRepo.findById(tarefaId);
+        if (todo.isPresent()){
+            this.todoRepo.deleteById(tarefaId);
+            return ResponseEntity.noContent().build();
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
